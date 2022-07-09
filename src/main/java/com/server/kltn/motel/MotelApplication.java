e87b24f748139249949c319384f6cb332e8fef6a
ngochoang0109 <hoangtn0109@gmail.com>
2022-07-09 14:17:45 +0700

    generate project

---
+++ src/main/java/com/server/kltn/motel/MotelApplication.java
@@ -0,0 +1,13 @@
+package com.server.kltn.motel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/kltn/motel/MotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.kltn.motel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
